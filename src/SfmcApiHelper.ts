6e838c02f8c4cc34f4eb1f21920ea01d85435922
Sivaskandharuban <65588953+Sivaskandharuban@users.noreply.github.com>
2020-12-02 10:46:20 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -27,6 +27,7 @@ export default class SfmcApiHelper
         };
 
         let postBody = {
+            'grant_type': 'refresh_token',
             'clientId': clientId,
             'clientSecret': clientSecret
         };
