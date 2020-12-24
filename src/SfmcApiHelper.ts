243b5532df5391b58cc7dfc41748f21341e0ce07
Sivaskandharuban <65588953+Sivaskandharuban@users.noreply.github.com>
2020-12-24 16:20:07 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -313,7 +313,7 @@ export default class SfmcApiHelper
                 errorMsg += "\nStatus: " + error.response ? error.response.status : "<None>";
                 errorMsg += "\nResponse data: " + error.response.data ? Utils.prettyPrintJson(JSON.stringify(error.response.data)) : "<None>";
                 Utils.logError(errorMsg);
-		    Utils.logError(JSON.stringify(error));
+		    Utils.prettyPrintJson(JSON.stringify(error));
 
                 reject(errorMsg);
 			});
