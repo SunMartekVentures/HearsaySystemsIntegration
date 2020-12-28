d6b861102abe6bfe952f73949dcd56482d91f6e1
Sivaskandharuban <65588953+Sivaskandharuban@users.noreply.github.com>
2020-12-28 18:29:56 +0530

    Update SfmcAppDemoRoutes.ts

--- src/SfmcAppDemoRoutes.ts
+++ src/SfmcAppDemoRoutes.ts
@@ -1,94 +1,18 @@
 'use strict';
 
+import path = require('path');
 import express = require("express");
-import jwt = require('jwt-simple');
 import SfmcApiHelper from './SfmcApiHelper';
 import Utils from './Utils';
 
-
-// <!-- Integrate an externally hosted app via iframe. -->
-export default class SfmcAppDemoRoutes
+export default class SfmcApiDemoRoutes
 {
     // Instance variables
     private _apiHelper = new SfmcApiHelper();
 
     /**
-     * login: called by Marketing Cloud when hosted app is launched. Decodes JWT in BODY passed by Marketing Cloud.
-     * Handles POST on: /login
-     * 
-     * Marketing Cloud does a POST on the '/login' route with the following JSON BODY:
-     * {
-     *  "jwt" : "<encoded JWT from SFMC>"
-     * }
-     * 
-     * More info: https://developer.salesforce.com/docs/atlas.en-us.mc-app-development.meta/mc-app-development/decode-jwt.htm
-     * More info: https://developer.salesforce.com/docs/atlas.en-us.mc-app-development.meta/mc-app-development/explanation-decoded-jwt.htm
-     * 
-     */
-    public login(req: express.Request, res: express.Response)
-    {
-        let self = this;
-        let sessionId = req.session.id;
-        Utils.logInfo("login called. SessionId = " + sessionId);
-
-        req.session.jwtFromSFMC = "";
-        req.session.decodedJWT = "";
-        req.session.refreshTokenFromJWT = "";
-        req.session.oauthAccessToken = "";
-        req.session.oauthAccessTokenExpiry = "";
-
-        // Decode JWT with the secret from environment variable.ggg
-        try
-        {
-            // Decode JWT
-            let encodedJWT = JSON.stringify(req.body.jwt);
-            let jwtSecret = process.env.DF18DEMO_JWTSECRET;
-            Utils.logInfo("Decoding JWT with secret from DF18DEMO_JWTSECRET = " + jwtSecret);
-            req.session.decodedJWT = jwt.decode(encodedJWT, jwtSecret, true); // pass 'noVerify = true' for this demo
-            
-            // Store JWT in the current session for redirect URL to pick up for display
-            req.session.jwtFromSFMC = Utils.prettyPrintJson(JSON.stringify(req.session.decodedJWT));
-            Utils.logInfo("Decoded JWT from SFMC = \n" + req.session.jwtFromSFMC);
-
-            // Get refreshToken from JWT and store in the current session for redirect URL to pick up for display
-            req.session.refreshTokenFromJWT = req.session.decodedJWT.request.rest.refreshToken;
-            Utils.logInfo("refreshToken from JWT = \n" + req.session.refreshTokenFromJWT);
-
-            //let redirectUrl = req.session.decodedJWT.request.application.redirectUrl;
-            //Utils.logInfo("Redirecting to: \n" + JSON.stringify(redirectUrl));
-            //res.redirect(redirectUrl); // redirect to MC app landing page
-        }
-        catch(error)
-        {
-            let errorMsg = "Error while decoding JWT. Message: " + error;
-            Utils.logError(errorMsg);
-            res.status(400).send(errorMsg);
-        }
-    }
-   
-    /**
-     * POST handler for: /logout
-     * logout: called by Marketing Cloud when user logs out
-     * 
-     */
-    public logout(req: express.Request, res: express.Response)
-    {
-        let sessionId = req.session.id;
-        Utils.logInfo("logout called. SessionId = " + sessionId);
-
-        // Clear out JWT and everything we got from it.
-        req.session.jwtFromSFMC = "";
-        req.session.decodedJWT = "";
-        req.session.refreshTokenFromJWT = "";
-        req.session.oauthAccessToken = "";
-        req.session.oauthAccessTokenExpiry = "";
-    
-        res.sendStatus(202); // accepted
-    }
-
-    /**
-     * GET handler for: /appdemooauthtoken
-     * getOAuthAccessToken: called by demo app to get an OAuth access token
+     * GET handler for /apidemooauthtoken
+     * getOAuthAccessToken: called by demo app to get an OAuth access token with ClientId/ClientSecret in environment variables
      * 
      * More info: https://developer.salesforce.com/docs/atlas.en-us.noversion.mc-getting-started.meta/mc-getting-started/get-access-token.htm
      * 
@@ -124,6 +48,7 @@ export default class SfmcAppDemoRoutes
                 req.session.oauthAccessToken = result.oauthAccessToken;
                 req.session.oauthAccessTokenExpiry = result.oauthAccessTokenExpiry;
                 res.status(result.status).send(result.statusText);
+                req.setTimeout(0, ()=>{});
 		})
             .catch((err) => {
                 res.status(500).send(err);
@@ -138,6 +63,33 @@ export default class SfmcAppDemoRoutes
         }
 		
 		//Utils.logInfo("called for data extension creation");
-		//self.loadData(req, res);
+		//		self.loadData(req, res);
 	}
+    
+    /**
+     * GET handler for /apidemoloaddata
+     * loadData: called by the demo app to load sample data into the Data Extension "DF18Demo";'
+     * 
+     * More info: https://developer.salesforce.com/docs/atlas.en-us.noversion.mc-apis.meta/mc-apis/postDataExtensionRowsetByKey.htm
+     * 
+     */
+    public loadData(req: express.Request, res: express.Response)
+    {
+        let self = this;
+        Utils.logInfo("loadData route entered.");
+        self._apiHelper.loadData(req, res);
+    }
+	
+	public loadDataForPage2(req: express.Request, res: express.Response)
+    {
+        let self = this;
+        Utils.logInfo("loadData route entered.");
+        self._apiHelper.loadDataForPage2(req, res);
+    }
+	public createDataExtension(req: express.Request, res: express.Response)
+    {
+        let self = this;
+        Utils.logInfo("Creating Data extension Dynamically.");
+        self._apiHelper.createDataExtension(req, res);
+    }
 }
