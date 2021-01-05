9bf92a53b5f8a957557c332f39f7ec1c70229fdf
Sivaskandharuban <65588953+Sivaskandharuban@users.noreply.github.com>
2021-01-05 13:02:45 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -117,7 +117,84 @@ export default class SfmcApiHelper
 	
 	public getCategoryID(req: express.Request, res: express.Response)
     {
+		Utils.logInfo("Get Category Method: " + this._oauthToken);
+			
+			
 		//Utils.logInfo("request body = " + JSON.stringify(req.body));
+		if (this._oauthToken!= "")
+        {
+            //Utils.logInfo("Using OAuth token: " + req.session.oauthAccessToken);
+            self.getCategoryIDHelper(this._oauthToken)
+            .then((result) => {
+                res.status(result.status).send(result.statusText);
+            })
+            .catch((err) => {
+                res.status(500).send(err);
+            });
+        }
+        else
+        {
+            // error
+            let errorMsg = "OAuth Access Token *not* found in session.\nPlease complete previous demo step\nto get an OAuth Access Token."; 
+            Utils.logError(errorMsg);
+            res.status(500).send(errorMsg);
+        }
+	}
+	public getCategoryIDHelper(oauthAccessToken: string) : Promise<any>
+	{
+		let soapMessage='<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">';
+				soapMessage+='<soapenv:Header>';
+				soapMessage+='<a:Action s:mustUnderstand="1">Retrieve</a:Action>';
+				soapMessage+='<a:MessageID>urn:uuid:e3f7fdfe-d225-47fb-a764-ad4f465fb724</a:MessageID>';
+				soapMessage+='<a:ReplyTo>';
+				soapMessage+='<a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address>';
+				soapMessage+='</a:ReplyTo>';
+				soapMessage+='<a:To s:mustUnderstand="1">https://webservice.exacttarget.com/Service.asmx</a:To>';
+				soapMessage+='<fueloauth>'+oauthAccessToken+'</fueloauth>'; 
+				soapMessage+='</soapenv:Header>';
+				soapMessage+='<soapenv:Body>';
+				soapMessage+='<RetrieveRequestMsg xmlns="http://exacttarget.com/wsdl/partnerAPI">';
+				soapMessage+='<RetrieveRequest>';
+				soapMessage+='<ObjectType>DataFolder</ObjectType>';
+				soapMessage+='<Properties>CustomerKey</Properties>';
+				soapMessage+='<Properties>ID</Properties>';              
+				soapMessage+='<Properties>Name</Properties>';
+				soapMessage+='<Properties>ParentFolder.ID</Properties>';            
+				soapMessage+='<Properties>ParentFolder.Name</Properties>';
+                soapMessage+='<Filter xsi:type="SimpleFilterPart">';
+                soapMessage+='<Property>Name</Property>';
+                soapMessage+='<SimpleOperator>equals</SimpleOperator>';
+                soapMessage+='<Value>Hearsay Integrations</Value>';
+				soapMessage+='</Filter>';
+				soapMessage+='</RetrieveRequest>';
+				soapMessage+='</RetrieveRequestMsg>';
+				soapMessage+='</soapenv:Body>';
+				soapMessage+='</soapenv:Envelope>';
+				
+	return new Promise<any>((resolve, reject) =>
+		{
+			let headers = {
+                'Content-Type': 'text/xml',
+                'SOAPAction': 'Retrieve'
+            };
+
+            // POST to Marketing Cloud Data Extension endpoint to load sample data in the POST body
+            axios.get('https://mcj6cy1x9m-t5h5tz0bfsyqj38ky.soap.marketingcloudapis.com/Service.asmx', soapMessage, {"headers" : headers})
+            .then((response: any) => {
+				Utils.logInfo(response.data);
+			})
+			.catch((error: any) => {
+										// error
+										let errorMsg = "Error loading sample data. POST response from Marketing Cloud:";
+										errorMsg += "\nMessage: " + error.message;
+										errorMsg += "\nStatus: " + error.response ? error.response.status : "<None>";
+										errorMsg += "\nResponse data: " + error.response.data ? Utils.prettyPrintJson(JSON.stringify(error.response.data)) : "<None>";
+										Utils.logError(errorMsg);
+
+										reject(errorMsg);
+									});
+			
+        });
 	}
 
     /**
