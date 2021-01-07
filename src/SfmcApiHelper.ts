c7361a55ef5c5e1f70847f1a51c484bdb72397e5
Sivaskandharuban <65588953+Sivaskandharuban@users.noreply.github.com>
2021-01-07 16:51:22 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -12,7 +12,6 @@ export default class SfmcApiHelper
     private _sfmcDataExtensionApiUrl = "https://mcj6cy1x9m-t5h5tz0bfsyqj38ky.rest.marketingcloudapis.com/hub/v1/dataevents/key:" + this._deExternalKey + "/rowset";
     private _oauthToken = "";
 	private FolderID='';
-	private parser = new DOMParser(); 
 	//private xmlDoc = '';
     
     
