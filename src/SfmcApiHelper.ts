a86d1ee6009bef4528d91e4d9269877a443b46df
SunMartekVentures <76422802+SunMartekVentures@users.noreply.github.com>
2021-01-20 12:05:03 +0530

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -198,8 +198,8 @@ export default class SfmcApiHelper
 				var parser = new xml2js.Parser();
 				let parsedResponse = parser.parseString(response.data, (err: any, result: { [x: string]: { [x: string]: { [x: string]: { [x: string]: any; }[]; }[]; }; }) => {
 				//this.validateStatus = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0]['OverallStatus'][0];
-				//Utils.logInfo('Validation Status : ' + this.validateStatus);
-				let validateDEName = JSON.stringify(result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0]['Results'][0]);
+				//Utils.logInfo('Validation Status : ' + response.data);
+				let validateDEName = result['soap:Envelope']['soap:Body'][0]['RetrieveResponseMsg'][0]['Results'][0]['Properties'][0]['Property'][0]['Value'][0]);
 				Utils.logInfo('Validated Data Extension Name : ' + validateDEName);
                 if(validateDEName){
 				resolve(
